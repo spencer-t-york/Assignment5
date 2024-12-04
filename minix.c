688d2740e0da081043f8eb00055515b6ae1fb518
Spencer York <spyork@spirit.eecs.csuohio.edu>
2024-12-03 21:42:04 -0500

    added support for minimount and miniumount

--- minix.c
+++ minix.c
@@ -1,25 +1,51 @@
 #include "traverse.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 // DEFINE FUNCTIONS
 // help
-void help();
-// minimount
-// miniumount
-// showsuper
+void help(); 		// lists all commands
 
-// traverse
+// minimount
+int minimount(char *); 	// 0 on success, 1 on error
+
+// miniumount
+int miniumount(int); 	// 0 on success, 1 on error
+
+// showsuper
+void showsuper();	// prints values of properties in superblock
+
 // showzone
+// -------- 		// not sure yet
+
 // showfile
-// quit
+// -------- 		// not sure yet
+
+// quit 		// this doesn't actually need a method
 
 // make the superblock struct
 // make the inode struct
 
 int main(int argc, char *argv[]) {
-  help();
-  traverse(argc, argv);
+    printf("\n...help()...\n");
+    help();
+    printf("\n...traverse()...\n");
+    //traverse(argc, argv);
+    printf("\n...minimount()...\n");
+    //int fd = minimount(argv[0]);
+    int fd = minimount("imagefile.img");
+    printf("FD: %d", fd);
+    printf("\n...miniumount()...\n");
+    if (fd == -1) {
+        printf("invalid file descriptor");
+    }
+    if (miniumount(fd) == 1) {
+        printf("miniumount() failed");
+    } else {
+        printf("miniumount() success! 👍");
+    }
 }
 
 
@@ -33,18 +59,39 @@ void help() {
   printf("\n  traverse      Lists the contents of the root directory");
   printf("\n  showzone      Displays the ASCII content of the specified zone number. Non-ASCII values are displayed as spaces");
   printf("\n  showfile      Displays the content of a specified file (in hex) located in the root directory. Assumes the file size is less than 7KB");
-  printf("\n   quit          Quits the minix console");
-
+  printf("\n  quit          Quits the minix console");
 }
+
 // minimount
-//     this opens the disk image file for reading with open()
-// miniumount
-//     this closes the disk image file using close(fd)
+// this opens the disk image file for reading with open()
+int minimount(char *filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        perror("failed to open disk image");
+        return -1;
+    }
+    return fd; // success
+}
+
+//  miniumount
+//  this closes the disk image file using close(fd)
+int miniumount(int fd) {
+    if (close(fd) == -1) {
+        return 1;
+    }
+    return 0; // success
+}
+
 // showsuper
 //     this just calls read(fd, superblock, sizeof(struct superblock));
 //     and then a bunch of print statements to print out all of the properties of the superblock
+void showsuper() {
+    printf("\nthis is from the superblock.\n");
+}
+
 // traverse
 //     I think this is just ls command which I've done already in CIS 340
+//     this is linked already in the traverse.c and .h file
 // showzone
 //     Not sure what this is yet...
 // showfile
